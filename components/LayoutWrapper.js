c4dc1954474e81cf98ea945db8cc5f744c3833e0
Omer Khan <mdomerkhan8000@gmail.com>
2023-07-28 00:49:19 +0530

    update

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -8,16 +8,16 @@ import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 
 const LayoutWrapper = ({ children }) => {
-  const underlineButtons = ['We are hiring!', 'Be our fan']
+  const underlineButtons = ['We are hiring!', 'Blog']
 
   return (
     <SectionContainer>
-      <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-10">
+      <div className="flex h-screen flex-col justify-between pt-0">
+        <header className="flex items-center justify-between py-2">
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
-                <div className="mr-3">
+                <div className="mr-2">
                   <Logo />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
@@ -37,8 +37,8 @@ const LayoutWrapper = ({ children }) => {
                   key={link.title}
                   href={link.href}
                   className={`p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4 ${
-                    link.title === 'Blog'
-                      ? 'rounded-md bg-black text-sm font-medium text-white sm:p-3'
+                    link.title === 'Be our early fan ->'
+                      ? 'rounded-md bg-black text-sm font-medium text-white sm:p-2'
                       : 'bg-transparent'
                   } ${underlineButtons.includes(link.title) ? 'underline' : ''}`}
                 >
--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ export default function Home({ posts }) {
                       <div className="text-base font-medium leading-6">
                         <Link
                           href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          className="text-black-500 hover:text-blue-600 dark:hover:text-primary-400"
                           aria-label={`Read "${title}"`}
                         >
                           Read more &rarr;
@@ -84,7 +84,7 @@ export default function Home({ posts }) {
         <div className="flex justify-end text-base font-medium leading-6">
           <Link
             href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            className="text-blue-500 hover:text-gray-600 dark:hover:text-primary-400"
             aria-label="all posts"
           >
             All Posts &rarr;
