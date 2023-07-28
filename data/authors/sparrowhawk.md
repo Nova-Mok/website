57edb22b651300e091ded1d29b503a669fc89a74
Omer Khan <mdomerkhan8000@gmail.com>
2023-07-28 10:12:52 +0530

    update

--- data/authors/sparrowhawk.md
+++ data/authors/sparrowhawk.md
@@ -1,8 +1,8 @@
 ---
 name: Neel Kamal
 avatar: /static/images/nk.jpeg
-occupation: Wizard of Earthsea
-company: Boost Up
+occupation: Founder
+company: BoostUp
 twitter: https://twitter.com/
 linkedin: https://www.linkedin.com/in/neelkamal1/
 ---
--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -29,7 +29,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
               type="text"
               onChange={(e) => setSearchValue(e.target.value)}
               placeholder="Search articles"
-              className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 transition-colors duration-150 focus:border-primary-500 focus:outline-none focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+              className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 transition-colors duration-150 focus:border-blue-500 focus:outline-none focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
             />
             <svg
               className="absolute right-3 top-3 h-5 w-5 cursor-pointer text-gray-400 transition-colors duration-150 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-200"
