96f7f4013eb2d6d221b83787391a2371064bded1
Omer Khan <mdomerkhan8000@gmail.com>
2023-07-31 10:33:49 +0530

    update

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -32,7 +32,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
               className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 transition-colors duration-150 focus:border-blue-500 focus:outline-none focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
             />
             <svg
-              className="absolute right-3 top-3 h-5 w-5 cursor-pointer text-gray-400 transition-colors duration-150 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-200"
+              className="text-black-500 absolute right-3 top-3 h-5 w-5 cursor-pointer transition-colors duration-150 hover:text-gray-600 dark:text-gray-300 dark:hover:text-gray-200"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
